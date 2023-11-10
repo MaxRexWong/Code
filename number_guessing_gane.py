300105523c505a7cfc17115ac6f25d107f48cdf6
MaxRexWong <150313417+MaxRexWong@users.noreply.github.com>
2023-11-10 12:46:37 +0000

    number_guessing_gane.py

--- number_guessing_gane.py
+++ number_guessing_gane.py
@@ -4,10 +4,21 @@ def game():
     num = random.randint(1,10)
     end = False
     count = 0
-    print('Guess a number!')
+    print('Guess a number from 1 to 9')
     while end == False:
+        check = False
         count += 1
-        ans = int(input())
+        if count == 5:
+            print('You loss')
+            break
+        ans = str(input())
+        while check == False:
+            if ans=='1'or ans=='2'or ans=='3'or ans=='4'or ans=='5'or ans=='6'or ans=='7'or ans=='8'or ans=='9':
+                ans = int(ans)
+                check = True
+            else:
+                print('Out of range! Enter again')
+                ans = str(input())
         if ans < num:
             print('Too small!')
         elif ans > num:
@@ -18,7 +29,7 @@ def game():
             end = True
 
 play = True
-print('Number guessing game')
+print('Number guessing game. You can guess 4 times')
 game()
 print('Wanna play again?')
 while play == True:
@@ -30,5 +41,4 @@ while play == True:
         play = False
     else:
         print('Invalid input! Try again')
-
 
