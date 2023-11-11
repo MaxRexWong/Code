d8714b444939964aa9ee6aba90fef4f668935b17
MaxRexWong <150313417+MaxRexWong@users.noreply.github.com>
2023-11-11 17:27:49 +0000

    password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,39 +1,44 @@
 import random
-up = ['A','B','C','D','E','F','G','H','I','J','K','L','N','M','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-lower = ['a','b','c','d','e','f','g','h','i','j','k','l','n','m','o','p','q','r','s','t','u','v','w','x','y','z']
-symb = ['!','@','#','$','%','?']
-psw = []
-end = False
 
-def gen(length):
+def gen():
+    up = ['A','B','C','D','E','F','G','H','I','J','K','L','N','M','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    low = ['a','b','c','d','e','f','g','h','i','j','k','l','n','m','o','p','q','r','s','t','u','v','w','x','y','z']
+    symb = ['!','@','#','$','%','?']
+    psw = []
+    while True:
+        try:
+            length = int(input('How long should your password be? Type a number\n'))
+        except ValueError:
+            print('Invalid input')
+            continue
+        break
     while length > 0:
-        i = random.randint(1,5)
-        if i == 1:
-            psw.append(up[random.randint(0,25)])
-        elif i == 2:
-            psw.append(lower[random.randint(0,25)])
-        elif i == 3:
-            psw.append(symb[random.randint(0,5)])
-        else:
+        i = random.randint(1,8)
+        if i <= 3:
             psw.append(random.randint(0,9))
+        elif i == 4 or i == 5:
+            psw.append(up[random.randint(0,25)])
+        elif i == 6 or i == 7:
+            psw.append(low[random.randint(0,25)])
+        else:
+            psw.append(symb[random.randint(0,5)])
         length -= 1
+    return psw
 
-def getLength():
-    length = int(input('Type the numeber of charaters your want in your password\n'))
-    return length
+def main():
+    while True:
+        psw = gen()
+        for elem in psw:
+            print(elem,end='')
+        print()
+        while True:
+             choice = input("Want another password?(yes/no)\n").lower()
+             if choice in ['yes', 'no']:
+                 break
+             else:
+                 print('Invalid input')
+        if choice == 'no':
+            print('Bye!')
+            break
 
-def getPassword():
-    gen(getLength())
-    for elem in psw:
-        print(elem,end='')
-
-while end == False:
-    getPassword()
-    print()
-    choice = input('Want another password? Type "yes" or "no"\n').lower()
-    if choice == 'yes':
-        psw = []
-    elif choice == 'no':
-        end = True
-    else:
-        print('Invaild input! Try again')
+main()
